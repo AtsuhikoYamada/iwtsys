d6c533b4c689048dc2bc9337cb8bcf7f60474071
Atsuhiko Yamada <atsu@ymd.gr.jp>
2023-02-20 15:28:08 +0900

    new file:   command.sh 	modified:   docker/web/package.json

---
+++ command.sh
@@ -0,0 +1,3 @@
+docker compose exec web php artisan make:model Person --migration
+docker compose exec web php artisan make:model Role --migration
+docker compose exec web php artisan make:model Office --migration
